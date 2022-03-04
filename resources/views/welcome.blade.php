310a6c781c5f4439f518dedae1c6520951b561a9
KCorigliano <corigliano98@hotmail.com>
2022-03-04 16:51:17 +0100

    add main style

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,15 +5,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
-    @foreach ($data as $movie)
-        <p>{{$movie["id"]}}</p>
-        <p>{{$movie["title"]}}</p>
-        <p>{{$movie["original_title"]}}</p>
-        <p>{{$movie["nationality"]}}</p>
-        <p>{{$movie["date"]}}</p>
-        <p>{{$movie["vote"]}}</p>
-    @endforeach
+    <div class="container">
+        <div class="row justify-content-between">
+            @foreach ($data as $movie)
+            <div class="card col-2 m-3 p-4">
+                <h5>Titolo: <span>{{$movie["title"]}}</span></h5>
+                <h5>Titolo originale: <span>{{$movie["original_title"]}}</span></h5>
+                <h5>Nazionalità: <span>{{$movie["nationality"]}}</span></h5>
+                <h5>Data di uscita: <span>{{$movie["date"]}}</span></h5>
+                <h5>Voto: <span>{{$movie["vote"]}}</span></h5>
+            </div>
+            @endforeach
+        </div>
+    </div>
 </body>
 </html>
